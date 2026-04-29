b80d3a08b4c730020f4b3d175cffc4da5c174442
prubiano <pierre.rubiano@swedbank.se>
2026-04-29 17:13:59 +0200

    Add SQL reference documentation

---
+++ streamlit_app/queries_reference.sql
@@ -0,0 +1,22 @@
+-- SQL Reference Documentation
+
+-- SELECT statement
+SELECT column1, column2 FROM table WHERE condition;
+
+-- INSERT statement
+INSERT INTO table (column1, column2) VALUES (value1, value2);
+
+-- UPDATE statement
+UPDATE table SET column1 = value1 WHERE condition;
+
+-- DELETE statement
+DELETE FROM table WHERE condition;
+
+-- JOIN statement
+SELECT column1, column2 FROM table1 JOIN table2 ON table1.id = table2.id;
+
+-- GROUP BY statement
+SELECT column, COUNT(*) FROM table GROUP BY column;
+
+-- ORDER BY statement
+SELECT column1, column2 FROM table ORDER BY column1 ASC;
